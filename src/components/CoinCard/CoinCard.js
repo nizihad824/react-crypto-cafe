f35d3569f63dbcc1596e8e1e363412d6d3988e9f
nizihad824 <mdnurulislam97@gmail.com>
2022-04-08 15:28:00 +0200

    display coin data

--- src/components/CoinCard/CoinCard.js
+++ src/components/CoinCard/CoinCard.js
@@ -2,9 +2,25 @@ import React from 'react';
 
 const CoinCard = ({coin}) => {
     return (
-        <div>
-            <p>id:{coin.id}</p>
-            <p>name:{coin.name}</p>
+        <div className='shadow-lg rounded-2xl w-[250px] bg-white p-4'>
+           <div className='gap-4 flex justify-between items-center'>
+          <div className='flex-shrink-0'>
+            <div>
+              <img
+                alt='profile'
+                src={coin.image}
+                className='mx-auto object-cover rounded-full h-16 w-16 '
+              />
+            </div>
+          </div>
+
+          <div>
+              <span>{coin.name}</span>
+              <span>{coin.symbol}</span>
+          </div>
+
+
+          </div> 
 
             
         </div>
